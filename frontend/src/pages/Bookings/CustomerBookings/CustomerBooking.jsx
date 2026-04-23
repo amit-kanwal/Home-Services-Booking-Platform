ac5131b966eef7c83a1a7b120ca4c38ece115857
amit-singh-kanwal <amitkanwal19@gmail.com>
2026-04-23 13:50:03 +0530

    Completed the customer booking page

--- frontend/src/pages/Bookings/CustomerBookings/CustomerBooking.jsx
+++ frontend/src/pages/Bookings/CustomerBookings/CustomerBooking.jsx
@@ -3,14 +3,14 @@ import BookingHistory from "../../../components/BookingHistory/BookingHistory.js
 import NavLogin from "../../../components/NavLogin/NavLogin";
 import api from "../../../Utils/axiosApi";
 import TabComponent from "../../../components/Tabs/TabComponent.jsx";
-import "./CustomerBookings.css"
+import "./CustomerBookings.css";
 import { Box } from "@mui/material";
 
 function CustomerBooking() {
-  const user = JSON.parse(localStorage.getItem('user'))
-  const customerId = user.id
+  const user = JSON.parse(localStorage.getItem("user"));
+  const customerId = user.id;
   const [booking, setBookingHistory] = useState([]);
-  const [tab, setTab] = useState('active')
+  const [tab, setTab] = useState("active");
 
   useEffect(() => {
     if (!customerId) return;
@@ -25,21 +25,29 @@ function CustomerBooking() {
       });
   }, [customerId]);
 
-  const filterBookings = booking.filter(
-    (item) => item.status === tab
-      
-  )
+  const filterBookings = booking.filter((item) => {
+    if (tab === "active") {
+      const bookingDate = new Date(item.booking_date);
+      const today = new Date();
+
+      const diffTime = today - bookingDate;
+      const diffDays = diffTime / (1000 * 60 * 60 * 24);
+
+      return item.status === "active" && diffDays <= 7;
+    }
+
+    return item.status === tab;
+  });
+
 
   return (
     <>
       <NavLogin />
-      <h2 className="booking-heading">
-        My Bookings
-      </h2>
-      <TabComponent tab={tab} setTab={setTab}/>
+      <h2 className="booking-heading">My Bookings</h2>
+      <TabComponent tab={tab} setTab={setTab} />
       <Box sx={{ mt: 3 }}>
-  <BookingHistory bookingHistory={filterBookings} />
-</Box>
+        <BookingHistory bookingHistory={filterBookings} setBookingHistory = {setBookingHistory}/>
+      </Box>
     </>
   );
 }
